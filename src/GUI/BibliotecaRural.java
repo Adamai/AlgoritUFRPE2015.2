4a2dbde8b5d9c85599640c12bdc61e12c9715b7b
Adamai <ropi2203@gmail.com>
2015-11-23 12:09:42 -0300

    Alterações na gui

--- src/GUI/BibliotecaRural.java
+++ src/GUI/BibliotecaRural.java
@@ -12,7 +12,7 @@ public class BibliotecaRural {
 		
 		ArvoreRB bib = new ArvoreRB();
 		ArvoreUsers cadastro = new ArvoreUsers();
-		User u = null;
+		 User u = null;
 		
 		int controlador = 1;
 		
@@ -38,9 +38,9 @@ public class BibliotecaRural {
 				String nome;
 				ArrayList<NodoLivro> emprestados;
 				boolean debito;
-				System.out.println("Digite seu nome"); // ADD INPUT EXCEPTION SÓ LETRAS
+				System.out.println("Digite seu CPF: "); // ADD INPUT EXCEPTION SÓ LETRAS
 				nome = scan5.nextLine();
-				
+				//buscar no repo se o CPF ja foi cadastrado, se sim, (pedir senha?) e logar como tal(transforma u)
 				u = new User(nome, null, false); //adicionar coisas de arvore (cor, pai, filhos)
 				cadastro.cadastrar(u);
 				System.out.println("Usuário cadastrado e logado como: " +nome);
@@ -58,12 +58,49 @@ public class BibliotecaRural {
 			}
 			
 			if(escolha == 3){
+				Scanner scan2 = new Scanner(System.in);
+				String titulo;
+				System.out.println("Digite o titulo do livro que está procurando");
+				titulo = scan2.next();
+					ArrayList<NodoLivro>livros = bib.busca(titulo); 
+					int aux = bib.busca(titulo).size();
+					for(int i=0;i<aux;i++){
+						System.out.println(livros.get(i));
+						//adicionar if para não encontrado
+					}
+					System.out.print("\n");
+					}
 				
+			if (escolha == 4)	{ //printa todos os usuarios e livros num for ou while
+				
+				
+			}
+			
+			if (escolha == 5){ //da baixa?
 				
 				
 			}
 			
 			
+			if (escolha == 6){ //remove usuario da arvore
+				
+				
+			}
+			
+			if(escolha == 7){ //remove livro da arvore
+				
+				
+				
+			}
+			
+			if(escolha == 8){
+				if(u == null)
+					System.out.println("Você não fez login!");
+				else{
+					u = null;
+		            System.out.println("Logoff realizado");	
+				}
+			}
 			
 			
 			
